465b924339176ab7f55b3db6635e7981169dbe14
EddieChoCho <chochoforwork@gmail.com>
2017-06-27 21:16:02 +0800

    modify signature

--- src/main/java/com/eddie/builder/AbstractPersonBuilder.java
+++ src/main/java/com/eddie/builder/AbstractPersonBuilder.java
@@ -5,9 +5,6 @@ import com.eddie.exception.RoleException;
 import com.eddie.model.enums.BasicRole;
 import com.eddie.model.enums.Role;
 
-/**
- * Created by ASUS on 2017/6/18.
- */
 public abstract class AbstractPersonBuilder {
 
     public void roleChecking(Role userRole, Role roleShouldBe) throws GuildSystemException {
